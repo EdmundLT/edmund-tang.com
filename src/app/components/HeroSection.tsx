d6922deb612bc5116c769c829a2c8ffd38c567ee
jerick2209 <edmundtlca@gmail.com>
2023-12-18 14:35:22 -0500

    animation

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,18 +1,33 @@
+"use client"
 import React from "react";
 import { AboutMe } from "./AboutMe";
+import FadeIn from "react-fade-in";
 const HeroSection = () => {
   return (
-    <div className="items-center py-12 px-4 mx-auto max-w-5xl lg:py-12 lg:px-6 text-slate-50" id="about">
+    <div
+      className="items-center py-12 px-4 mx-auto max-w-5xl lg:py-12 lg:px-6 text-slate-50"
+      id="about"
+    >
       <div className="mx-auto py-32 items-center flex-col">
-        <div className="pt-32">
-          <h1 className="my-4 text-5xl font-bold leading-tight">EDMUND TANG</h1>
-          <p className="text-2xl mb-8">
-            Profressional Front End Developer & Tech Enthusiast
-          </p>
-        </div>
+        <FadeIn delay={250}>
+          <div className="pt-32">
+            <h1 className="my-4 text-5xl font-bold leading-tight">
+              EDMUND TANG
+            </h1>
+            <p className="text-2xl mb-8">
+              Profressional Front End Developer & Tech Enthusiast
+            </p>
+          </div>
+        </FadeIn>
         <AboutMe />
         <p className="text-xl">
-          Get in touch 👉🏻 <a href="mailto:edmundtl.work@gmail.com" className="underline decoration-pink-500 hover:bg-pink-500 hover:duration-700 p-2 rounded-lg">edmundtl.work@gmail.com</a>
+          Get in touch 👉🏻{" "}
+          <a
+            href="mailto:edmundtl.work@gmail.com"
+            className="underline decoration-pink-500 hover:bg-pink-500 hover:duration-700 p-2 rounded-lg"
+          >
+            edmundtl.work@gmail.com
+          </a>
         </p>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,42 @@
+"use client";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
+
 import { Skills } from "./components/Skills";
 import HeroSection from "./components/HeroSection";
 import AnimatedCursor from "react-animated-cursor";
 import Projects from "./components/Projects";
 import { Experiences } from "./components/Experiences";
+
+const boxVariant = {
+  visible: { opacity: 1, scale: 1, transition: { duration: 1 } },
+  hidden: { opacity: 0, scale: 0 },
+};
+
+const Box = ({ Component }: any) => {
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    }
+  }, [control, inView]);
+
+  return (
+    <motion.div
+      className="box"
+      ref={ref}
+      variants={boxVariant}
+      initial="hidden"
+      animate={control}
+    >
+      <Component />
+    </motion.div>
+  );
+};
+
 export default function Home() {
   return (
     <>
@@ -28,9 +62,10 @@ export default function Home() {
         ]}
       />
       <HeroSection />
-      <Skills />
-      <Experiences />
-      <Projects />
+      {/* <Box Component={HeroSection} /> */}
+      <Box Component={Skills} />
+      <Box Component={Experiences} />
+      <Box Component={Projects} />
     </>
   );
 }
