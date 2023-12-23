c27f9f17b62f6afe6e38f65faa4007e15b5b6aeb
jerick2209 <edmundtlca@gmail.com>
2023-12-23 02:02:31 -0500

    notion CMS

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { IoIosArrowForward } from "react-icons/io";
+import { getAllPublished } from "../../../lib/notion";
 
-const Home = () => {
+const Home = async () => {
+  const data = await getAllPublished();
   return (
     <div className="items-center py-12 px-4 mx-auto max-w-5xl lg:py-16 lg:px-6 text-slate-50">
       {/* Title and section */}
@@ -15,8 +17,26 @@ const Home = () => {
       {/* Blogs Section */}
       <div className="py-12">
         {/* Blog Itself */}
-
-        <div className=" space-y-4 select-none hover:bg-slate-300 hover:bg-opacity-10 py-4 px-2 rounded-2xl hover:duration-300">
+        {data.map((blog: any) => {
+          return (
+            <div
+              key={blog.id}
+              className=" space-y-4 select-none hover:bg-slate-300 hover:bg-opacity-10 py-4 px-2 rounded-2xl hover:duration-300"
+            >
+              <p className="text-lg font-bold text-slate-200">{blog.title}</p>
+              <p className="text-slate-400 font-light"> {blog.date}</p>
+              <p className="max-w-3xl text-lg font-light text-slate-300">
+                {blog.description}
+              </p>
+              <div className="pt-2">
+                <a className="flex" href={`/blogs/${blog.slug}`}>
+                  <IoIosArrowForward size={20} /> Read More
+                </a>
+              </div>
+            </div>
+          );
+        })}
+        {/* <div className=" space-y-4 select-none hover:bg-slate-300 hover:bg-opacity-10 py-4 px-2 rounded-2xl hover:duration-300">
           <p className="text-lg font-bold text-slate-200">
             How Next.js replace React.js in my work?
           </p>
@@ -32,9 +52,7 @@ const Home = () => {
               <IoIosArrowForward size={20} /> Read More
             </a>
           </div>
-        </div>
-
-
+        </div> */}
       </div>
     </div>
   );
